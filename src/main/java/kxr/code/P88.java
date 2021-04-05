ff447905cc6cad38c6135ced6bf7795aaf10b656
root <1254122559@qq.com>
2021-04-05 11:56:38 +0800

    21/4/4 逆向双指针 合并两个有序数组

---
+++ src/main/java/kxr/code/P88.java
@@ -0,0 +1,42 @@
+package kxr.code;
+
+import java.util.Arrays;
+
+/**
+ * @Author: kxr
+ * @Date: 2021/4/5
+ * @Description
+ *
+ * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+ *
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+ *
+ * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * 输出：[1,2,2,3,5,6]
+ *
+ * 输入：nums1 = [1], m = 1, nums2 = [], n = 0
+ * 输出：[1]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class P88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int tail = nums1.length - 1;
+        int p1 = m - 1;
+        int p2 = n - 1;
+        while (p2 >= 0) {
+            if (p1 < 0 || nums1[p1] <= nums2[p2]) {
+                nums1[tail--] = nums2[p2--];
+            } else {
+                nums1[tail--] = nums1[p1--];
+            }
+        }
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public static void main(String[] args) {
+        new P88().merge(new int[]{1,2,3,0,0,0},3,new int[]{2,5,6},3);
+    }
+}
